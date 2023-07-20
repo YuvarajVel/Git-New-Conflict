cdd51674b1acace66abfbd9b248008a4dabcf5f0
YuvarajVel <135580694+YuvarajVel@users.noreply.github.com>
2023-07-20 18:05:13 +0530

    new

---
+++ src/org/phone/Phone.java
@@ -0,0 +1,24 @@
+package org.phone;
+
+public class Phone {
+private void phoneInfo(int b) {
+	// TODO Auto-generated method stub
+System.out.println("RAM : "+b);
+}
+private void phoneInfo(int c,long s) {
+	// TODO Auto-generated method stub
+System.out.println("Storage : "+c+"\nPin : "+s);
+}
+private void phoneInfo(long l,float f,String s,boolean b,char c) {
+	// TODO Auto-generated method stub
+	System.out.println("PH: "+l+"\npi value="+f+"\nPhone:"+s+"\nStatement is "+b+"\tType"+c);
+
+}
+public static void main(String[] args) {
+	Phone p=new Phone();
+	p.phoneInfo(8);
+	p.phoneInfo(64,5678);
+	p.phoneInfo(96675678868l,3.14f,"Oneplus",true, 'C');
+	
+}
+}
